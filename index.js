41f6aa98606a059e52f9c3081b6cc9cb585b3ffb
Microtechbymak26 <usubka786@Gmail.com>
2024-04-01 04:33:52 -0700

    ....

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// /usr/bin/env node
+#! /usr/bin/env node
 import inquirer from "inquirer";
 // 1) computer will generate a random number --- Done
 // 2) user input for guessing game --- Done
 // 3) compare user input with computer generated number and show result --- Done
 const randomNumber = Math.floor(Math.random() * 6 + 1);
-// console.log(randomNumber);
+console.log("Welcome to number guessing game");
 const answers = await inquirer.prompt([
     {
         name: "userGussedNumber",
